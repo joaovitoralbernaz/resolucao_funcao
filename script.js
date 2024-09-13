859482d7a3f5ef5d6ee92b064addfdd0d3436576
joaovitoralbernaz <159267082+joaovitoralbernaz@users.noreply.github.com>
2024-09-13 15:14:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 function calcula() {
-    let a = document.getElementById('a').value;
-    let b = document.getElementById('b').value;
-    let c = document.getElementById('c').value;
-    let delta = (b * b) * ( - 4 * a * c);
-    let raiz_delta = Math.sqrt(delta);
-
+    const a = parseFloat(document.getElementById('a').value);
+    const b = parseFloat(document.getElementById('b').value);
+    const c = parseFloat(document.getElementById('c').value);
+    let x1;
+    let x2;
+    const delta = (b*b*(-4*a*c));
+    const raiz_delta = Math.sqrt(delta);
+    
+    if (delta < 0) {
+        alert("Não há raizes reais");
+    }if (delta > 0) {
+        x1 = (-b + raiz_delta)/(2*a);
+        x2 = (-b - raiz_delta)/(2*a);
+        document.getElementById('x1').innerText = x1;
+        document.getElementById('x2').innerText = x2;
+        
+    }
     document.getElementById('delta').innerText = raiz_delta;
 }
